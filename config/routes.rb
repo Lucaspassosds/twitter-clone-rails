1cdcc60b4e4e1fc584bc40657c258dd2a5c51a40
Lucas Passos <lsiqueira355@gmail.com>
2023-03-06 16:59:55 -0300

    initial UI

--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,6 @@ Rails.application.routes.draw do
 
 
   devise_for :users
-  root to: 'home#index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :tweets, except: [:edit, :update]
+  root to: 'tweets#index'
 end
