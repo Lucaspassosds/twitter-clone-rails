766ad9f118f6e93f0031ac4e1377a5d2a425e190
Lucas Passos <lsiqueira355@gmail.com>
2023-03-06 20:45:39 -0300

    removed template tables

---
+++ db/migrate/20230306232840_drop_tables.rb
@@ -0,0 +1,10 @@
+class DropTables < ActiveRecord::Migration[7.0]
+  def change
+    drop_table :pay_customers
+    drop_table :pay_charges
+    drop_table :pay_merchants
+    drop_table :pay_payment_methods
+    drop_table :pay_subscriptions
+    drop_table :pay_webhooks
+  end
+end
